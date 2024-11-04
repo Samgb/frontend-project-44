414d6b3e66ff9b649a89d9b014ed203aedc3d10d
alexandrgrebenyuk <layout>
2024-11-04 17:14:14 +0300

    brain game gcd

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 [![Maintainability](https://api.codeclimate.com/v1/badges/996be0a17ce00ba2473f/maintainability)](https://codeclimate.com/github/Samgb/frontend-project-44/maintainability)
 
 https://asciinema.org/a/0JmEfvjE1GQgOmNRMC2RwbXnf
+
+[//]: # (Brain-calc)
+https://asciinema.org/connect/63ee445a-e79e-4696-9a16-bd4e9b17cc08
---
+++ bin/brain-gcd.js
@@ -0,0 +1,41 @@
+import {
+   numberIsEven,
+   askRightAnswer,
+   randomOperator,
+   calculations,
+   generateRandomNum,
+   askMessage, isCorrect
+} from "../src/index.js";
+import askName, {inputHistory} from "../src/cli.js";
+
+askName();
+
+console.log('Find the greatest common divisor of given numbers.');
+
+const brainGcd = (num1, num2, count = 0) => {
+
+   if (count >= 3) {
+      return console.log('Congratulations, ' + inputHistory.name + '!');
+   }
+
+   const operator = randomOperator();
+   console.log(`Question: ${num1} ${num2}`);
+
+   const answer = askMessage(`Your answer: `);
+   const decision = (a, b) => {
+      if (b === 0) {
+         return a;
+      }
+      return decision(b, a % b);
+   }
+
+   if (isCorrect(answer, decision(num1, num2))) {
+      console.log('Correct!');
+      brainGcd(generateRandomNum(), generateRandomNum(), count + 1);
+   } else {
+      return console.log(`'${answer}' is wrong answer ;(. Correct answer was '${decision(num1, num2)}'. \n Let's try again, ${inputHistory.name}!`);
+   }
+}
+
+brainGcd(generateRandomNum(), generateRandomNum(), 0);
+
